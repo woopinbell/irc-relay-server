f46d29cecf18a84820f49b119a2374d327f2e9d8
seungwoo7050 <seungwoo7050@naver.com>
2025-05-02 14:02:40 +0900

    feat(client): 닉네임 색인 관리

--- src/ClientRegistry.cpp
+++ src/ClientRegistry.cpp
@@ -1,5 +1,7 @@
 #include "ClientRegistry.hpp"
 
+#include "Channel.hpp"
+
 ClientState::ClientState()
     : fd(-1),
       passOk(false),
@@ -35,10 +37,30 @@ std::vector<int> ClientRegistry::fds() const {
     return values;
 }
 
+int ClientRegistry::findFdByNickname(const std::string& nickname) const {
+    const std::map<std::string, int>::const_iterator it =
+        _nicknameIndex.find(Channel::canonicalNick(nickname));
+    return it == _nicknameIndex.end() ? -1 : it->second;
+}
+
+void ClientRegistry::setNickname(int fd, const std::string& nickname) {
+    const std::string canonical = Channel::canonicalNick(nickname);
+    ClientState& client = state(fd);
+    if (!client.nick.empty()) {
+        _nicknameIndex.erase(Channel::canonicalNick(client.nick));
+    }
+    client.nick = nickname;
+    client.hasNick = true;
+    _nicknameIndex[canonical] = fd;
+}
+
 void ClientRegistry::erase(int fd) {
     std::map<int, ClientState>::iterator it = _states.find(fd);
     if (it == _states.end()) {
         return;
+    }
+    if (!it->second.nick.empty()) {
+        _nicknameIndex.erase(Channel::canonicalNick(it->second.nick));
     }
     _states.erase(it);
 }
