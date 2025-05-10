dd58472d37a35e110eb41a576bdfe03c109204ad
seungwoo7050 <seungwoo7050@naver.com>
2025-05-10 13:19:45 +0900

    feat(room): 채널 구성원 조회 구현

--- src/IrcApplication.hpp
+++ src/IrcApplication.hpp
@@ -49,6 +49,7 @@ private:
     void handleInvite(int fd, const IrcMessage& message);
     void handleMode(int fd, const IrcMessage& message);
     void handleList(int fd, const IrcMessage& message);
+    void handleNames(int fd, const IrcMessage& message);
     void handleChannelMode(int fd, const IrcMessage& message);
 
     Channel& ensureChannel(const std::string& name);
