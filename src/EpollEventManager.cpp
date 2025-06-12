37ff1e33d97e7c27fa0a1257207639ffe5bc2d0a
seungwoo7050 <seungwoo7050@naver.com>
2025-06-12 09:18:05 +0900

    feat(event): epoll 준비 이벤트 변환 구현

--- src/EpollEventManager.cpp
+++ src/EpollEventManager.cpp
@@ -3,8 +3,10 @@
 #include "EventManager.hpp"
 
 #include <sys/epoll.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
+#include <array>
 #include <cerrno>
 #include <cstdint>
 #include <system_error>
@@ -26,6 +28,16 @@ int nativeEventsFor(EventInterest interests)
         events |= EPOLLOUT;
     }
     return events;
+}
+
+int socketErrorFor(int fd)
+{
+    int error = 0;
+    socklen_t length = sizeof(error);
+    if (::getsockopt(fd, SOL_SOCKET, SO_ERROR, &error, &length) == -1) {
+        return errno;
+    }
+    return error;
 }
 
 class EpollEventManager : public EventManager {
@@ -94,6 +106,46 @@ public:
         interests_.erase(found);
     }
 
+    std::vector<Event> wait(int timeoutMs) override
+    {
+        std::array<struct epoll_event, 128> nativeEvents;
+        const int count =
+            ::epoll_wait(epollFd_, nativeEvents.data(), nativeEvents.size(), timeoutMs);
+
+        if (count == -1) {
+            if (errno == EINTR) {
+                return std::vector<Event>();
+            }
+            throw std::system_error(errno, std::generic_category(), "epoll_wait");
+        }
+
+        std::vector<Event> events;
+        events.reserve(static_cast<std::size_t>(count));
+        for (int i = 0; i < count; ++i) {
+            const uint32_t native = nativeEvents[static_cast<std::size_t>(i)].events;
+
+            Event event;
+            event.fd = nativeEvents[static_cast<std::size_t>(i)].data.fd;
+            event.error = (native & EPOLLERR) != 0;
+            event.hangup = (native & EPOLLHUP) != 0;
+#ifdef EPOLLRDHUP
+            event.hangup = event.hangup || ((native & EPOLLRDHUP) != 0);
+#endif
+            if (event.error) {
+                event.errorCode = socketErrorFor(event.fd);
+            }
+            if ((native & EPOLLIN) != 0) {
+                event.interests |= EventInterest::Read;
+            }
+            if ((native & EPOLLOUT) != 0) {
+                event.interests |= EventInterest::Write;
+            }
+
+            events.push_back(event);
+        }
+        return events;
+    }
+
 private:
     int epollFd_;
     std::unordered_map<int, EventInterest> interests_;
@@ -111,6 +163,12 @@ private:
 };
 
 } // namespace
+
+std::unique_ptr<EventManager> EventManager::createDefault()
+{
+    return std::unique_ptr<EventManager>(new EpollEventManager());
+}
+
 } // namespace irc
 
 #endif
